1a0de4144797330166bdbbf19d8a26fb2c46ad23
huutam1991 <huutam1991@gmail.com>
2024-06-02 13:46:27 +0800

    Strategy: correct method init()

--- server_app/src/strategy/strategy.cpp
+++ server_app/src/strategy/strategy.cpp
@@ -11,32 +11,36 @@ void Strategy::init()
     SimpleGuard g(m_is_init);
 
     // Load current strategy info
-    DataModel config = DataModel::get_data_model_list(STRATEGY_DB_NAME, "config")[0];
-
-    m_symbol = std::string(config["symbol"]);
-    m_volumn = config["volumn"];
-    m_move_price = config["move_price"];
-    m_sell_buy_ratio = config["sell_buy_ratio"];
-    m_is_running = config["is_running"];
-
-    // Log config
-    ADD_LOG("Strategy config:");
-    ADD_LOG("- symbol: " << m_symbol);
-    ADD_LOG("- volumn: " << m_volumn);
-    ADD_LOG("- move_price: " << m_move_price);
-    ADD_LOG("- sell_buy_ratio: " << m_sell_buy_ratio);
-    ADD_LOG("- is_running: " << m_is_running);
-
-    // Load checkpoints
-    m_checkpoints = std::make_shared<CheckPoints>(m_symbol, m_volumn, m_move_price, m_sell_buy_ratio);
-
-    // Add price callback + subscribe to symbol
-    auto gateway = GatewayManager::instance().get_gateway(GatewayEnum::BINANCE);
-    gateway->register_price_update([this](double price)
+    std::vector<DataModel> configs = DataModel::get_data_model_list(STRATEGY_DB_NAME, "config");
+    if (configs.size() > 0)
     {
-        this->update(price);
-    });
-    gateway->subscribe_symbol(m_symbol);
+        DataModel config = configs[0];
+
+        m_symbol = std::string(config["symbol"]);
+        m_volumn = config["volumn"];
+        m_move_price = config["move_price"];
+        m_sell_buy_ratio = config["sell_buy_ratio"];
+        m_is_running = config["is_running"];
+
+        // Log config
+        ADD_LOG("Strategy config:");
+        ADD_LOG("- symbol: " << m_symbol);
+        ADD_LOG("- volumn: " << m_volumn);
+        ADD_LOG("- move_price: " << m_move_price);
+        ADD_LOG("- sell_buy_ratio: " << m_sell_buy_ratio);
+        ADD_LOG("- is_running: " << m_is_running);
+
+        // Load checkpoints
+        m_checkpoints = std::make_shared<CheckPoints>(m_symbol, m_volumn, m_move_price, m_sell_buy_ratio);
+
+        // Add price callback + subscribe to symbol
+        auto gateway = GatewayManager::instance().get_gateway(GatewayEnum::BINANCE);
+        gateway->register_price_update([this](double price)
+        {
+            this->update(price);
+        });
+        gateway->subscribe_symbol(m_symbol);
+    }
 }
 
 void Strategy::on_config_change()
