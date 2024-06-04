7053f2f7832be480696604be78288f341ccd0397
huutam1991 <huutam1991@gmail.com>
2024-06-04 20:40:46 +0800

    Implement method StrategyStateStop::get_close_sell_perpetual_order_by_checkpoint()

--- server_app/src/strategy/strategy_state/strategy_state_stop.cpp
+++ server_app/src/strategy/strategy_state/strategy_state_stop.cpp
@@ -52,6 +52,33 @@ void StrategyStateStop::run(double price)
         //     };
         // });
 
+        // Send close perpetual order
+        Order close_sell_perpetual = get_close_sell_perpetual_order_by_checkpoint(checkpoint);
+        AppUtils::instance().get_app_pool()->execute_function([gateway = m_gateway, close_sell_perpetual, checkpoint]()
+        {
+            DataModel cp = checkpoint;
+
+            // Place close buy spot order
+            Json response = gateway->place(close_sell_perpetual);
+
+            // // Calculate profit
+            // double place_volumn_in_usdt = cp["positions"]["buy_spot"]["volumn_in_usdt"];
+            // double close_volumn_in_usdt = response["volumn_in_usdt"];
+            // double profit = close_volumn_in_usdt - place_volumn_in_usdt;
+
+            // // Save profit to checkpoint
+            // double buy_spot_profit = cp["buy_spot_profit"];
+            // double total_profit = cp["total_profit"];
+            // cp["accounting"]["buy_spot_profit"] = buy_spot_profit + profit;
+            // cp["accounting"]["total_profit"] = total_profit + profit;
+
+            // Close buy spot position
+            cp["positions"]["sell_perpetual"] = Json{
+                {"quantity", 0.0},
+                {"volumn_in_usdt", 0.0},
+            };
+        });
+
         // Mark current checkpoint is false
         checkpoint["is_current_checkpoint"] = false;
     }
@@ -76,7 +103,7 @@ Order StrategyStateStop::get_close_buy_spot_order_by_checkpoint(DataModel& check
 Order StrategyStateStop::get_close_sell_perpetual_order_by_checkpoint(DataModel& checkpoint)
 {
     std::string symbol = checkpoint["info"]["symbol"];
-    double quantity = checkpoint["positions"]["sell_perpetual"];
+    double quantity = checkpoint["positions"]["sell_perpetual"]["quantity"];
     double round_up_quantity = m_gateway->round_up_quantity("perpetual", symbol, quantity);
 
     return Order(
