a8c6f7c16a7f033233def5c9a4035f515d9b0113
huutam1991 <huutam1991@gmail.com>
2025-02-18 15:09:29 +0700

    Add class MeasureTime

---
+++ core/include/measure_time.h
@@ -0,0 +1,64 @@
+#ifndef MEASURE_TIME
+#define MEASURE_TIME
+
+#include <chrono>
+#include <util_macros.h>
+
+enum MeasureUnit
+{
+    SECOND,
+    MILLISECOND,
+    MICROSECOND,
+    NANOSECOND
+};
+
+class MeasureTime
+{
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    MeasureUnit m_measure_unit;
+
+public:
+    MeasureTime(MeasureUnit measure_unit = MeasureUnit::MILLISECOND) : m_measure_unit{measure_unit}
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    ~MeasureTime()
+    {
+        auto end = std::chrono::high_resolution_clock::now();
+        size_t duration_count = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        double execute_time;
+        std::string unit;
+
+        switch (m_measure_unit)
+        {
+        case MeasureUnit::SECOND:
+            execute_time = (double)duration_count / 1000000000.0;
+            unit = "seconds";
+            break;
+        case MeasureUnit::MILLISECOND:
+            execute_time = (double)duration_count / 1000000.0;
+            unit = "milliseconds";
+            break;
+        case MeasureUnit::MICROSECOND:
+            execute_time = (double)duration_count / 1000.0;
+            unit = "microseconds";
+            break;
+        case MeasureUnit::NANOSECOND:
+            execute_time = (double)duration_count / 1.0;
+            unit = "nanoseconds";
+            break;
+
+        // Default is millisecond
+        default:
+            execute_time = (double)duration_count / 1000000.0;
+            unit = "milliseconds";
+            break;
+        }
+
+        ADD_LOG("Execute time: " << execute_time << " " << unit);
+    }
+};
+
+#endif //MEASURE_TIME
