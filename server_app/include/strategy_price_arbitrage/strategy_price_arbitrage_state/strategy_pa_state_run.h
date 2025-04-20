708be22e910f4047626d4c6c03a1196735ccbaa9
huutam1991 <huutam1991@gmail.com>
2025-04-20 21:05:06 +0800

    Implement method handle_price_update()

--- server_app/include/strategy_price_arbitrage/strategy_price_arbitrage_state/strategy_pa_state_run.h
+++ server_app/include/strategy_price_arbitrage/strategy_price_arbitrage_state/strategy_pa_state_run.h
@@ -2,6 +2,9 @@
 #define STRATEGY_PA_STATE_RUN_H
 
 #include <strategy_price_arbitrage/strategy_price_arbitrage_state/strategy_pa_state.h>
+#include <unordered_map>
+
+#include <order/order_manager.h>
 
 class StrategyPriceArbitrageStateRun : public StrategyPriceArbitrageState
 {
@@ -11,6 +14,16 @@ public:
     virtual void begin();
     virtual void end();
     virtual TaskVoid run(StrategyData data);
+
+    TaskVoid handle_price_update(double price);
+    TaskVoid handle_order_update(Order& order);
+
+private:
+    Order m_current_order;
+    std::unordered_map<OrderId, Order> m_current_open_orders;
+
+    void remove_open_order_id(OrderId order_id);
+    Order get_limit_buy_spot_order_by_price(double price);
 };
 
 #endif //STRATEGY_PA_STATE_RUN_H
--- server_app/src/strategy_price_arbitrage/strategy_price_arbitrage_state/strategy_pa_state_run.cpp
+++ server_app/src/strategy_price_arbitrage/strategy_price_arbitrage_state/strategy_pa_state_run.cpp
@@ -1,5 +1,9 @@
 #include <strategy_price_arbitrage/strategy_price_arbitrage_state/strategy_pa_state_run.h>
 
+#define PRICE_DELTA 5
+#define TOO_LOW_PRICE_DELTA 220
+#define TOO_HIGH_PRICE_DELTA 190
+
 StrategyPriceArbitrageStateRun::StrategyPriceArbitrageStateRun(std::shared_ptr<Gateway>& gateway, StrategyPriceArbitrageConfig& config)
     : StrategyPriceArbitrageState(gateway, config)
 {
@@ -18,9 +22,91 @@ void StrategyPriceArbitrageStateRun::end()
     m_gateway->cancel_all(m_config.get_symbol());
 }
 
+void StrategyPriceArbitrageStateRun::remove_open_order_id(OrderId order_id)
+{
+    if (m_current_open_orders.find(order_id) != m_current_open_orders.end())
+    {
+        m_current_open_orders.erase(order_id);
+    }
+}
+
+Order StrategyPriceArbitrageStateRun::get_limit_buy_spot_order_by_price(double current_price)
+{
+    std::string symbol = m_config.get_symbol();
+    double price = current_price - m_config.buy_at_lower_price;
+    double quantity = m_config.buy_volumn / price;
+    double round_up_quantity = m_gateway->round_up_quantity("spot", symbol, quantity);
+
+    return Order(
+        OrderManager::instance().generate_order_id(),
+        Order::ExchangeType::SPOT,
+        Order::Status::NOT_AVAILABLE,
+        symbol,
+        Order::Side::BUY,
+        Order::OrderType::LIMIT,
+        price,
+        round_up_quantity
+    );
+}
+
+TaskVoid StrategyPriceArbitrageStateRun::handle_price_update(double price)
+{
+    // Place new order if current price is moving > PRICE_DELTA compare to current order's price
+    if (std::abs(m_current_order.price - (price - m_config.buy_at_lower_price)) > PRICE_DELTA)
+    {
+        Order order = get_limit_buy_spot_order_by_price(price);
+        m_current_order = co_await m_gateway->place(order, Order::Status::NEW);
+
+        // Insert to [m_current_open_orders]
+        m_current_open_orders.insert(std::make_pair(m_current_order.order_id, m_current_order));
+    }
+
+    // Cancel all of orders that price is too low or too high
+    for (auto& [order_id, order] : m_current_open_orders)
+    {
+        if (order.price <= price - TOO_LOW_PRICE_DELTA || order.price >= price - TOO_HIGH_PRICE_DELTA)
+        {
+            m_gateway->cancel(order);
+        }
+    }
+
+    co_return;
+}
+
+TaskVoid StrategyPriceArbitrageStateRun::handle_order_update(Order& order)
+{
+    // NEW - do nothing
+    if (order.status == Order::Status::NEW)
+    {}
+    // FILLED - update data to order's checkpoint
+    else if (order.status == Order::Status::FILLED)
+    {
+        // TBD
+    }
+    // CANCELED - update [order_id] = 0 for order's checkpoint
+    else if (order.status == Order::Status::CANCELED)
+    {
+        remove_open_order_id(order.order_id);
+    }
+
+    co_return;
+}
+
 TaskVoid StrategyPriceArbitrageStateRun::run(StrategyData data)
 {
     ADD_LOG("StrategyPriceArbitrageStateRun - run");
+
+    double price;
+    if (std::holds_alternative<double>(data))
+    {
+        double price = std::get<double>(data);
+        co_await handle_price_update(price);
+    }
+    else
+    {
+        Order order = std::get<Order>(data);
+        co_await handle_order_update(order);
+    }
 
     co_return;
 }
