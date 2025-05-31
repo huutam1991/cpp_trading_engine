9cc82ceec71f07d7eeb75e84b264d9835571c936
huutam1991 <huutam1991@gmail.com>
2025-05-31 18:59:04 +0800

    Add class SavableObject

---
+++ core/data_model/savable_object.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <data_model/data_model.h>
+
+template<class T>
+class SavableObject
+{
+    T m_object;
+    DataModel m_data_model;
+    
+    std::string m_id = "-1";
+    std::string m_db;
+    std::string m_collection;
+    
+    SavableObject();
+
+public:
+    // SavableObject(const std::string& db, const std::string& collection)
+    //     : m_db{db}, m_collection{collection}
+    // {}
+
+    Json to_json()
+    {
+        return m_object.to_json();
+    }
+
+    from_json(Json& data)
+    {
+        return T::from_json(data);
+    }
+
+    static SavableObject load_single_object(const std::string& db, const std::string& collection)
+    {
+        SavableObject res;
+        res.m_db = db;
+        res.m_collection = collection;
+        res.m_data_model = DataModel::load_single_data_model(db, collection);
+        res.m_object = T::from_json(res.m_data_model.get_data());
+
+        return res;
+    }
+
+    template<class Key>
+    static std::unordered_map<Key, SavableObject> load_objects_map(const std::string& db, const std::string& collection, const std::string& key_field_name)
+    {
+        std::unordered_map<Key, DataModel> data_list = DataModel::load_data_model_map(db, collection, key_field_name);
+
+        std::unordered_map<Key, SavableObject> res;
+        for (auto& [key, dm] : data_list)
+        {
+            SavableObject object;
+            object.m_db = db;
+            object.m_collection = collection;
+            object.m_data_model = dm;
+            object.m_object = T::from_json(dm.get_data());
+
+            res.insert(std::make_pair(key, object));
+        }
+
+        return res;
+    }
+
+};
