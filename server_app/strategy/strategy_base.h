03e39f12bb7ede00141878f1e9e1a08790bf7bb4
huutam1991 <huutam1991@gmail.com>
2025-06-04 20:44:05 +0800

    Add method StrategyBase::on_config_change()

--- server_app/strategy/strategy_base.h
+++ server_app/strategy/strategy_base.h
@@ -1,16 +1,19 @@
 #pragma once
 
 #include <data_model/savable_object.h>
+#include <coroutine/event_base_manager.h>
 
+#include <app_constants.h>
 #include <strategy/strategy_abstract.h>
 #include <strategy/strategy_state_base.h>
 
-template<class Strategy, class StrategyConfig, const char* StrategyDBName>
+template<class Strategy, class StrategyConfig, const char* StrategyDBName, EventBaseID eventBaseID>
 class StrategyBase : public StategyAbstract
 {
     SavableObject<StrategyConfig> m_config;
     SavableObject<StrategyStateData> m_current_state;
     std::unordered_map<StrategyState, StrategyStateBase*> m_states;
+    EventBase* m_event_base;
     StrategyState m_previous_state;
     bool m_is_config_updating = false;
 
@@ -18,9 +21,16 @@ public:
     StrategyBase() : 
         m_config{SavableObject<StrategyConfig>::load_single_object(StrategyDBName, "config")},
         m_current_state{SavableObject<StrategyStateData>::load_single_object(StrategyDBName, "state")},
-        m_states{StrategyBase::init_states()}
+        m_states{StrategyBase::init_states()},
+        m_event_base{EventBaseManager::get_event_base_by_id(eventBaseID)}
     {
         m_previous_state = m_current_state.object.state;
+    }
+
+    void on_config_change(StrategyConfig new_config)
+    {
+        m_config = new_config;
+        update_config().start_running_on(m_event_base);
     }
     
     TaskVoid update(StrategyUpdateData data) override
@@ -56,4 +66,7 @@ public:
 
         co_return;
     }
+
+protected:
+    virtual TaskVoid update_config(StrategyConfig config); 
 };
