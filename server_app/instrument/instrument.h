c726c9ce119f410ed314559f541b0eed53e883b6
huutam1991 <huutam1991@gmail.com>
2025-06-10 16:02:37 +0800

    Add class Instrument

---
+++ server_app/instrument/instrument.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+#include <json/json.h>
+
+struct Instrument
+{
+    std::string symbol;
+    std::string exchange_id;
+    size_t lot_size;
+
+    Json to_json()
+    {
+        return {
+            {"symbol", symbol},
+            {"exchange_id", exchange_id},
+            {"lot_size", lot_size}
+        };
+    }
+
+    static Instrument from_json(Json& data)
+    {
+        return Instrument {
+            (std::string)data["symbol"], 
+            (std::string)data["exchange_id"],
+            (size_t)data["lot_size"]
+        };
+    };
+};
