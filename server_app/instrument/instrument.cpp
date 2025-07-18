d78e98c530f4a0a25aab6d26d50e37de88d5a708
huutam1991 <huutam1991@gmail.com>
2025-07-18 14:43:58 +0800

    Implement caching mechanism for instruments in the Instrument class

--- server_app/instrument/instrument.cpp
+++ server_app/instrument/instrument.cpp
@@ -40,3 +40,116 @@ double Instrument::get_round_up_quantity(double quantity)
     std::string round_str_number = round_string_number(std::to_string(quantity), lot_size);
     return std::stod(round_str_number);
 }
+
+Instrument::CacheInstruments& Instrument::get_cache_instruments(ExchangeId exchange_id)
+{
+    static std::unordered_map<ExchangeId, CacheInstruments> cache_instruments_by_exchange;
+    static std::mutex cache_instruments_mutex;
+
+    std::unique_lock<std::mutex> lock(cache_instruments_mutex);
+
+    // Check if already loaded
+    if (cache_instruments_by_exchange.find(exchange_id) == cache_instruments_by_exchange.end())
+    {
+        std::string exchange_name(enum_reflect::enum_name(exchange_id));
+        CacheInstruments cache_instruments = SavableObject<Instrument>::load_objects_map<std::string>(INSTRUMENT_DB_NAME, exchange_name, "symbol");
+        cache_instruments_by_exchange.emplace(exchange_id, std::move(cache_instruments));
+    }
+
+    return cache_instruments_by_exchange[exchange_id];
+}
+
+Instrument::CacheInstruments& Instrument::load_cache_instruments(ExchangeId exchange_id)
+{
+    static std::unordered_map<ExchangeId, CacheInstruments> cache_instruments_by_exchange;
+    static std::mutex cache_instruments_mutex;
+
+    CacheInstruments& cache_instruments = get_cache_instruments(exchange_id);
+
+    // Clear existing instruments for this exchange
+    clear_instrument_by_exchange(exchange_id);
+
+    // Add instruments to the static list
+    for (auto& [_, instrument] : cache_instruments)
+    {
+        add_instrument_to_list(exchange_id, instrument.object);
+    }
+
+    return cache_instruments;
+}
+
+void Instrument::add_instrument_to_cache(ExchangeId exchange_id, const Instrument& instrument)
+{
+    CacheInstruments& cache_instruments = load_cache_instruments(exchange_id);
+    std::string exchange_name(enum_reflect::enum_name(exchange_id));
+    std::string symbol = instrument.symbol.to_string();
+    
+    // Add to cache
+    auto [it, success] = cache_instruments.emplace(symbol, SavableObject<Instrument>(INSTRUMENT_DB_NAME, exchange_name));
+    if (success == false)
+    {
+        it->second = instrument; // Update to DB
+    }
+    else
+    {
+        throw std::runtime_error("Cannot add instrument to cache: [" + symbol + "]");
+    }
+
+    // Add to static list
+    add_instrument_to_list(exchange_id, instrument);
+}
+
+void Instrument::clear_instrument_by_exchange(ExchangeId exchange_id)
+{
+    for (size_t i = 0; i < InstrumentType::TOTAL; i++)
+    {
+        for (size_t j = 0; j < StoreType::TOTAL; j++)
+        {
+            get_instrument_list(exchange_id, (InstrumentType)i, (StoreType)j).clear();
+        }
+    }
+}
+
+void Instrument::add_instrument_to_list(ExchangeId exchange_id, const Instrument& instrument)
+{
+    for (size_t i = 0; i < InstrumentType::TOTAL; i++)
+    {
+        std::unordered_map<std::string, const Instrument*>& ins_by_symbol = get_instrument_list(exchange_id, (InstrumentType)i, StoreType::BY_SYMBOL);
+        std::unordered_map<std::string, const Instrument*>& ins_by_exchange_symbol = get_instrument_list(exchange_id, (InstrumentType)i, StoreType::BY_EXCHANGE_SYMBOL);
+
+        ins_by_symbol.emplace(instrument.symbol, &instrument);
+        ins_by_exchange_symbol.emplace(instrument.exchange_symbol, &instrument);
+    }
+} 
+
+const Instrument* Instrument::get_instrument_by_symbol(ExchangeId exchange_id, InstrumentType instrument_type, const std::string& symbol)
+{
+    std::unordered_map<std::string, const Instrument*>& instruments = get_instrument_list(exchange_id, instrument_type, StoreType::BY_SYMBOL);
+    auto it = instruments.find(symbol);
+    if (it != instruments.end())
+    {
+        return it->second;
+    }
+    else
+    {
+        throw std::runtime_error("Cannot find instrument with symbol: [" + symbol + "]");
+    }
+
+    return nullptr;
+}
+
+const Instrument* Instrument::get_instrument_by_exchange_symbol(ExchangeId exchange_id, InstrumentType instrument_type, const std::string& symbol)
+{
+    std::unordered_map<std::string, const Instrument*>& instruments = get_instrument_list(exchange_id, instrument_type, StoreType::BY_EXCHANGE_SYMBOL);
+    auto it = instruments.find(symbol);
+    if (it != instruments.end())
+    {
+        return it->second;
+    }
+    else
+    {
+        throw std::runtime_error("Cannot find instrument with exchange symbol: [" + symbol + "]");
+    }
+
+    return nullptr;
+}
--- server_app/instrument/instrument.h
+++ server_app/instrument/instrument.h
@@ -1,26 +1,32 @@
 #pragma once
 
 #include <string>
+#include <mutex>
 
 #include <json/json.h>
 #include <symbol/symbol.h>
+#include <utils/spin_lock.h>
 #include <enum_reflect/enum_reflect.h>
 #include <data_model/savable_object.h>
+#include <app_constants.h>
 
 enum ExchangeId
 {
     BINANCE,
-    COINBASE
+    COINBASE,
+    TOTAL
 };
 
 enum InstrumentType
 {
     SPOT,
-    PERPETUAL
+    PERPETUAL,
+    TOTAL
 };
 
-struct Instrument
+class Instrument
 {
+public:
     ExchangeId exchange_id;
     InstrumentType instrument_type;
     Symbol symbol;
@@ -35,73 +41,30 @@ struct Instrument
     std::string round_string_number(const std::string& str_number, size_t precision);
     double get_round_up_quantity(double quantity);
 
+    using CacheInstruments = std::unordered_map<std::string, SavableObject<Instrument>>;
+    
+    static CacheInstruments& load_cache_instruments(ExchangeId exchange_id);
+    static void add_instrument_to_cache(ExchangeId exchange_id, const Instrument& instrument);
+    static const Instrument* get_instrument_by_symbol(ExchangeId exchange_id, InstrumentType instrument_type, const std::string& symbol);
+    static const Instrument* get_instrument_by_exchange_symbol(ExchangeId exchange_id, InstrumentType instrument_type, const std::string& symbol);
+
+private:
     enum StoreType
     {
         BY_SYMBOL,
         BY_EXCHANGE_SYMBOL,
         TOTAL
     };
-    
-    static std::unordered_map<ExchangeId, std::unordered_map<std::string, Instrument>>& get_instrument_list(StoreType store_type)
+
+    static CacheInstruments& get_cache_instruments(ExchangeId exchange_id);
+    static void clear_instrument_by_exchange(ExchangeId exchange_id);
+    static void add_instrument_to_list(ExchangeId exchange_id, const Instrument& instrument);
+
+    static std::unordered_map<std::string, const Instrument*>& get_instrument_list(ExchangeId exchange_id, InstrumentType instrument_type, StoreType store_type)
     {
-        using InstrumentStoreType = std::unordered_map<ExchangeId, std::unordered_map<std::string, Instrument>>;
-        static InstrumentStoreType instrument_list[StoreType::TOTAL];
+        using InstrumentList = std::unordered_map<std::string, const Instrument*>;
+        static InstrumentList instrument_list[ExchangeId::TOTAL][InstrumentType::TOTAL][StoreType::TOTAL];
 
-        return instrument_list[store_type];
-    }
-    
-    static std::unordered_map<std::string, Instrument>& get_instrument_list_by_exchange(ExchangeId exchange_id, StoreType store_type)
-    {
-        auto& instrument_list = get_instrument_list(store_type);
-
-        if (instrument_list.find(exchange_id) == instrument_list.end())
-        {
-            instrument_list.insert(std::make_pair(exchange_id, std::unordered_map<std::string, Instrument>()));
-        }
-
-        return instrument_list.find(exchange_id)->second;
-    }
-
-    static void create_instrument_list_by_exchange_symbol(ExchangeId exchange_id)
-    {
-        std::unordered_map<std::string, Instrument>& ins_by_symbol = get_instrument_list_by_exchange(exchange_id, StoreType::BY_SYMBOL);
-        std::unordered_map<std::string, Instrument>& ins_by_exchange_symbol = get_instrument_list_by_exchange(exchange_id, StoreType::BY_EXCHANGE_SYMBOL);
-
-        for (auto& [_, instrument] : ins_by_symbol)
-        {
-            ins_by_exchange_symbol.insert(std::make_pair(instrument.exchange_symbol, instrument));
-        }
-    }
-
-    static Instrument* get_instrument_by_symbol(ExchangeId exchange_id, const std::string& symbol)
-    {
-        std::unordered_map<std::string, Instrument>& instruments = get_instrument_list_by_exchange(exchange_id, StoreType::BY_SYMBOL);
-        auto it = instruments.find(symbol);
-        if (it != instruments.end())
-        {
-            return &it->second;
-        }
-        else
-        {
-            throw std::runtime_error("Cannot find instrument with symbol: [" + symbol + "]");
-        }
-
-        return nullptr;
-    }
-
-    static Instrument* get_instrument_by_exchange_symbol(ExchangeId exchange_id, const std::string& symbol)
-    {
-        std::unordered_map<std::string, Instrument>& instruments = get_instrument_list_by_exchange(exchange_id, StoreType::BY_EXCHANGE_SYMBOL);
-        auto it = instruments.find(symbol);
-        if (it != instruments.end())
-        {
-            return &it->second;
-        }
-        else
-        {
-            throw std::runtime_error("Cannot find instrument with exchange symbol: [" + symbol + "]");
-        }
-
-        return nullptr;
+        return instrument_list[exchange_id][instrument_type][store_type];
     }
 };
