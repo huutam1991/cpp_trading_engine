9a97b0d33ffe6a60b080a79c5e91fbd28ef1c466
huutam1991 <huutam1991@gmail.com>
2025-07-25 21:02:24 +0800

    Refactor JsonObjectTest; uncomment and enhance test logic for JsonNew object

--- test_cases/core/json/test_json_object.cpp
+++ test_cases/core/json/test_json_object.cpp
@@ -1,12 +1,10 @@
 #include <gtest/gtest.h>
-// #include <c_json/json.h>
+#include <c_json/json.h>
 
 TEST(JsonObjectTest, Basic)
 {
     std::cout << "Running JsonObjectTest" << std::endl;
-    // JsonNew j;
-    // j[1] = "hello";
-    EXPECT_EQ(1, 1);
-    // EXPECT_EQ((std::string)j[1], "hello");
-    // EXPECT_EQ((std::string)j[1], "hello2");
+    JsonNew j;
+    j[1] = "hello";
+    EXPECT_EQ((std::string)j[1], "hello");
 }
