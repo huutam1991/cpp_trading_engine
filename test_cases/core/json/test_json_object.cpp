643353b177eb8d8658f13d4e10c2d757b799d2a7
huutam1991 <huutam1991@gmail.com>
2025-07-25 21:00:25 +0800

    Refactor test setup; remove test_main.cpp and consolidate test sources in CMakeLists.txt for improved organization

---
+++ test_cases/core/json/test_json_object.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+// #include <c_json/json.h>
+
+TEST(JsonObjectTest, Basic)
+{
+    std::cout << "Running JsonObjectTest" << std::endl;
+    // JsonNew j;
+    // j[1] = "hello";
+    EXPECT_EQ(1, 1);
+    // EXPECT_EQ((std::string)j[1], "hello");
+    // EXPECT_EQ((std::string)j[1], "hello2");
+}
