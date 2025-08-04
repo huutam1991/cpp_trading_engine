a8e4d4d17e5eb8c2ec176af4111a71691a2e4063
huutam1991 <huutam1991@gmail.com>
2025-08-04 18:38:19 +0800

    Refactor Task and BaseTask structures to improve coroutine handling and promise management

--- core/coroutine/task.h
+++ core/coroutine/task.h
@@ -3,151 +3,30 @@
 #include <future>
 #include "task_void.h"
 #include "base_promise_type.h"
+#include "base_task.h"
 
 template<class T>
-struct Task
+struct Task : public BaseTask<T>
 {
-    struct promise_type : public BasePromiseType
+    struct promise_type : public BaseTask<T>::promise_type
     {
         // Methods of a standard promise
         Task get_return_object()
         {
-            return Task{std::coroutine_handle<promise_type>::from_promise(*this)};
+            return Task{this};
         }
-        std::suspend_always initial_suspend() { return {}; }
-        std::suspend_always final_suspend() noexcept
-        {
-            if (m_suspending_promise != nullptr)
-            {
-                m_suspending_promise->set_waiting(false);
-            }
 
-            return {};
-        }
         void return_value(T v)
         {
-            promise_value.set_value(v);
-            value = v;
+            this->promise_value.set_value(v);
+            this->value = v;
         }
-        void unhandled_exception() { std::terminate(); }
-
-        // Main value
-        T value;
-
-        // Promise value
-        std::promise<T> promise_value;
     };
 
-    std::coroutine_handle<promise_type> handle = nullptr;
-    Task(std::coroutine_handle<promise_type> h) : handle(h) {}
-    Task() {};
-    Task(const Task& copy) = delete;
-    Task(Task&& copy) : handle{std::move(copy.handle)} { copy.handle = nullptr; }
-    ~Task()
-    {
-        // Light destroy, lol
-        destroy(false);
-    }
-
-    bool operator==(std::nullptr_t null) const
-    {
-        return handle == nullptr;
-    }
-
-    Task& operator=(const Task& copy) = delete;
-
-    Task& operator=(Task&& copy)
-    {
-        if (handle != nullptr)
-        {
-            destroy();
-        }
-
-        handle = std::move(copy.handle);
-        copy.handle = nullptr;
-        return *this;
-    }
-
-    void destroy(bool complete = true)
-    {
-        // This is just a Task object with nullptr handle, not a really Task that is created by C++
-        if (handle == nullptr)
-        {
-            return;
-        }
-
-        if (handle.done() || complete == true)
-        {
-            auto base_promise_type = get_base_promise_type();
-            if (base_promise_type->m_event_base != nullptr)
-            {
-                uint64_t task_id = base_promise_type->task_id;
-                base_promise_type->m_event_base->remove_from_event_base(task_id);
-            }
-
-            handle.destroy();
-        }
-        else
-        {
-            // Mark this task is already release, then it will be destroy later when it's done
-            get_base_promise_type()->is_task_release = true;
-        }
-    }
-
-    T value()
-    {
-        return handle.promise().value;
-    }
-
-    // Get BasePromiseType of current coroutine
-    BasePromiseType* get_base_promise_type()
-    {
-        promise_type& promise = handle.promise();
-        return &promise;
-    }
-
-    void save_suspending_promise(BasePromiseType* suspend_base_pt)
-    {
-        get_base_promise_type()->m_suspending_promise = suspend_base_pt;
-    }
-
-    void register_on(EventBase* event_base)
-    {
-        auto base_promise_type = get_base_promise_type();
-        base_promise_type->register_on(event_base, handle);
-    }
-
-    std::future<T> start_running_on(EventBase* event_base)
-    {
-        register_on(event_base);
-
-        // Return future
-        promise_type& promise = handle.promise();
-        return promise.promise_value.get_future();
-    }
-
-    bool await_ready()
-    {
-        return handle.done();
-    }
-
-    template<class promise_type>
-    void await_suspend(std::coroutine_handle<promise_type> suspend_handle)
-    {
-        // Tricky here, cast promise_type to a pointer of BasePromiseType (suppose all of promise_type is child class of BasePromiseType class)
-        promise_type& promise = suspend_handle.promise();
-        BasePromiseType *suspend_base_pt = &promise;
-        suspend_base_pt->set_waiting(true);
-
-        // Save to suspending_promise
-        save_suspending_promise(suspend_base_pt);
-
-        // Running this task on EventBase
-        register_on(suspend_base_pt->m_event_base);
-    }
-
-    T await_resume()
-    {
-        return handle.promise().value;
-    }
+    Task(promise_type* promise) : BaseTask<T>(promise) {}
+    Task() = default;
+    Task(const Task&) = delete;
+    Task(Task&&) = default;
+    Task& operator=(const Task&) = delete;
+    Task& operator=(Task&&) = default;
 };
