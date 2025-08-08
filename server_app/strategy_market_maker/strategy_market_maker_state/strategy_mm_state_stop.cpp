e25f8496cdf1b40c7d60e2d6af773b5cf18cbd3b
huutam1991 <huutam1991@gmail.com>
2025-08-08 20:18:32 +0800

    Refactor logging in StrategyMarketMakerStateStop::update for improved clarity and consistency

--- server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.cpp
+++ server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.cpp
@@ -20,10 +20,22 @@ Task<void> StrategyMarketMakerStateStop::update(StrategyUpdateData data)
     if (std::holds_alternative<PriceUpdate>(data))
     {
         price_update = std::get<PriceUpdate>(data);
+        spdlog::info("StrategyBuySpotStateStop: do nothing, symbol: {}, price: {} ", price_update.instrument->symbol, price_update.price);
     }
+    else if (std::holds_alternative<OrderBookSnapShot*>(data))
+    {
+        OrderBookSnapShot* snapshot = std::get<OrderBookSnapShot*>(data);
+        double bid_price = snapshot->get_best_bid();
+        double ask_price = snapshot->get_best_ask();
+        double ask_quantity = snapshot->get_best_ask_quantity();
+        double bid_quantity = snapshot->get_best_bid_quantity();
 
-    spdlog::info("StrategyMarketMakerStateStop: do nothing, symbol: {}, price: {} ", price_update.instrument->symbol, price_update.price);
+        spdlog::info("StrategyBuySpotStateStop: do nothing, symbol: {}, bid_price: {}, ask_price: {}", snapshot->instrument->symbol, bid_price, ask_price);
+        spdlog::info("StrategyBuySpotStateStop: do nothing, symbol: {}, bid_quantity: {}, ask_quantity: {}", snapshot->instrument->symbol, bid_quantity, ask_quantity);
 
+        // Release the snapshot back to the pool
+        OrderBookSnapShotPool::release(snapshot);
+    }
     co_return;
 }
 
