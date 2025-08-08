7c57e4148bb8be0753ffc73070c6bf93dc137b66
huutam1991 <huutam1991@gmail.com>
2025-08-08 15:11:24 +0800

    Update EventBaseID reference in StrategyMeanReversion to use MEAN_REVERSION_STRATEGY

--- server_app/app_constants.h
+++ server_app/app_constants.h
@@ -207,7 +207,7 @@ enum EventBaseID
     ORDER,                    // OrderManager
     DB_HELPER,                // Help to data to MongoDB
     GATEWAY,                  // Gateway
-    STRATEGY,
+    MEAN_REVERSION_STRATEGY,  // Mean Reversion Strategy
     BUY_SPOT_STRATEGY,        // Strategy - Buy Spot
     PRICE_ARBITRAGE_STRATEGY, // Strategy - Price Arbitrage
     MARKET_MAKER_STRATEGY     // Strategy - Market Maker
