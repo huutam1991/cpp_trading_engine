f6256a815056597bf53ebe3892e1ad212db028d0
huutam1991 <huutam1991@gmail.com>
2025-08-11 18:01:31 +0800

    Add widen and tight parameters to StrategyMarketMakerConfig and update JSON serialization

--- server_app/strategy_market_maker/strategy_market_maker_config.h
+++ server_app/strategy_market_maker/strategy_market_maker_config.h
@@ -11,6 +11,8 @@ struct StrategyMarketMakerConfig
     double price_step_between_blocks = 100.0;
     int orders_each_side_per_block = 5;
     double inventory_skew_ratio = 3.0;
+    double widen = 1.8;
+    double tight = 0.6;
     bool is_running = false;
 
     Json to_json()
@@ -22,6 +24,8 @@ struct StrategyMarketMakerConfig
             {"price_step_between_blocks", price_step_between_blocks},
             {"orders_each_side_per_block", orders_each_side_per_block},
             {"inventory_skew_ratio", inventory_skew_ratio},
+            {"widen", widen},
+            {"tight", tight},
             {"is_running", is_running},
         };
     }
@@ -39,6 +43,8 @@ struct StrategyMarketMakerConfig
             res.price_step_between_blocks = (double)data["price_step_between_blocks"];
             res.orders_each_side_per_block = (int)data["orders_each_side_per_block"];
             res.inventory_skew_ratio = (double)data["inventory_skew_ratio"];
+            res.widen = (double)data["widen"];
+            res.tight = (double)data["tight"];
             res.is_running = (bool)data["is_running"];
         }
 
