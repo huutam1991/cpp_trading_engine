5196258d7db7373b132690f6952f0b8629654317
huutam1991 <huutam1991@gmail.com>
2025-09-16 16:16:02 +0800

    Refactor StrategyMarketMakerStateStop: remove OrderGap structure and related logic

--- server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.cpp
+++ server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.cpp
@@ -1,9 +1,7 @@
 #include "strategy_mm_state_stop.h"
 #include <chrono>
 
-StrategyMarketMakerStateStop::StrategyMarketMakerStateStop() :
-    m_db_name{enum_reflect::enum_name(EventBaseID::MARKET_MAKER_STRATEGY)},
-    m_order_gap_list{SavableObject<OrderGap>::load_objects_map<size_t>(m_db_name, "order_gap", "time")}
+StrategyMarketMakerStateStop::StrategyMarketMakerStateStop()
 {
 }
 
@@ -20,16 +18,6 @@ void StrategyMarketMakerStateStop::end()
 Json StrategyMarketMakerStateStop::get_info()
 {
     Json gap_list;
-    for (const auto& [id, object] : m_order_gap_list)
-    {
-        gap_list.push_back(object.to_json());
-    }
-
-    gap_list.sort([](Json& a, Json& b) -> bool
-    {
-        return (size_t)a["time"] > (size_t)b["time"];
-    });
-
     return gap_list;
 }
 
@@ -51,25 +39,8 @@ Task<void> StrategyMarketMakerStateStop::update(StrategyUpdateData data)
 
         spdlog::info("StrategyMarketMakerStateStop: do nothing, symbol: {}, bid_price: {}, ask_price: {}", snapshot->instrument->symbol, bid_price, ask_price);
         spdlog::info("StrategyMarketMakerStateStop: do nothing, symbol: {}, bid_quantity: {}, ask_quantity: {}", snapshot->instrument->symbol, bid_quantity, ask_quantity);
-
-        // Release the snapshot back to the pool
-        OrderBookSnapShotPool::release(snapshot);
-
-        // Check to save gap price
-        if (ask_price - bid_price > 5.0)
-        {
-            OrderGap gap;
-            gap.bid = bid_price;
-            gap.ask = ask_price;
-            gap.gap = ask_price - bid_price;
-            gap.bid_quantity = bid_quantity;
-            gap.ask_quantity = ask_quantity;
-            gap.time = OrderManager::instance().generate_order_id(); // Order Id is current time in nanoseconds
-
-            SavableObject<OrderGap> object(m_db_name, "order_gap", gap);
-            m_order_gap_list.insert(std::make_pair(gap.time, object));
-        }
     }
+
     co_return;
 }
 
--- server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.h
+++ server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.h
@@ -5,48 +5,9 @@
 #include <strategy/strategy_state_base.h>
 #include <gateways/gateway.h>
 #include <order/order_manager.h>
-#include <strategy_price_arbitrage/strategy_price_arbitrage_config.h>
 
 class StrategyMarketMakerStateStop : public StrategyStateBase
 {
-    struct OrderGap
-    {
-        double bid;
-        double ask;
-        double gap;
-        double bid_quantity;
-        double ask_quantity;
-        size_t time;
-
-        Json to_json() const
-        {
-            Json json;
-            json["bid"] = bid;
-            json["ask"] = ask;
-            json["gap"] = gap;
-            json["bid_quantity"] = bid_quantity;
-            json["ask_quantity"] = ask_quantity;
-            json["time"] = time;
-
-            return json;
-        }
-
-        static OrderGap from_json(Json& data)
-        {
-            OrderGap gap;
-            gap.bid = data["bid"];
-            gap.ask = data["ask"];
-            gap.gap = data["gap"];
-            gap.bid_quantity = data["bid_quantity"];
-            gap.ask_quantity = data["ask_quantity"];
-            gap.time = data["time"];
-
-            return gap;
-        }
-    };
-
-    std::string m_db_name;
-    std::unordered_map<size_t, SavableObject<OrderGap>> m_order_gap_list;
 
 public:
     StrategyMarketMakerStateStop();
