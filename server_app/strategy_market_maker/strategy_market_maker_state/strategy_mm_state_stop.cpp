317c3a91d0a484c9f4f79e35b59019cb51725507
huutam1991 <huutam1991@gmail.com>
2025-09-16 18:08:43 +0800

    Enhance update method in StrategyMarketMakerStateStop to log TradeUpdate details

--- server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.cpp
+++ server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_stop.cpp
@@ -29,6 +29,11 @@ Task<void> StrategyMarketMakerStateStop::update(StrategyUpdateData data)
         price_update = std::get<PriceUpdate>(data);
         spdlog::info("StrategyMarketMakerStateStop: do nothing, symbol: {}, price: {} ", price_update.instrument->symbol, price_update.price);
     }
+    else if (std::holds_alternative<TradeUpdate>(data))
+    {
+        TradeUpdate trade = std::get<TradeUpdate>(data);
+        spdlog::info("StrategyMarketMakerStateStop: do nothing, symbol: {}, trade_id: {}, price: {}, quantity: {}", trade.instrument->symbol, trade.trade_id, trade.price, trade.quantity);
+    }
     else if (std::holds_alternative<OrderBookSnapShot*>(data))
     {
         OrderBookSnapShot* snapshot = std::get<OrderBookSnapShot*>(data);
