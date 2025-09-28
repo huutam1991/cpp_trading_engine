f2b9de31b872b3c36e2b9d7c8a45503443e25be4
huutam1991 <huutam1991@gmail.com>
2025-09-28 15:34:33 +0800

    Remove price_step from StrategyMarketMakerConfig and replace it with min_trade_volume in JSON serialization and deserialization

--- server_app/strategy_market_maker/strategy_market_maker_config.h
+++ server_app/strategy_market_maker/strategy_market_maker_config.h
@@ -8,7 +8,7 @@ struct StrategyMarketMakerConfig
     std::string symbol = "BTC-USDC-PERPETUAL";
     double volumn = 0.1;
     double price_gap = 35.0;
-    double price_step = 3.0;
+    double min_trade_volume = 2.0;
     double clear_orders_gap = 800.0;
     bool is_running = false;
 
@@ -18,7 +18,7 @@ struct StrategyMarketMakerConfig
             {"symbol", symbol},
             {"volumn", volumn},
             {"price_gap", price_gap},
-            {"price_step", price_step},
+            {"min_trade_volume", min_trade_volume},
             {"clear_orders_gap", clear_orders_gap},
             {"is_running", is_running},
         };
@@ -34,7 +34,7 @@ struct StrategyMarketMakerConfig
             res.symbol = (std::string)data["symbol"];
             res.volumn = (double)data["volumn"];
             res.price_gap = (double)data["price_gap"];
-            res.price_step = (double)data["price_step"];
+            res.min_trade_volume = (double)data["min_trade_volume"];
             res.clear_orders_gap = (double)data["clear_orders_gap"];
             res.is_running = (bool)data["is_running"];
         }
