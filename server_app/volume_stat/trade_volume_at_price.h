a98f149fae6d985b571e17d1d31656a2cd277cc9
huutam1991 <huutam1991@gmail.com>
2025-09-28 16:18:24 +0800

    Fix total_sell_volume calculation in add_volume method to ensure correct volume accounting for sell trades

--- server_app/volume_stat/trade_volume_at_price.h
+++ server_app/volume_stat/trade_volume_at_price.h
@@ -23,7 +23,7 @@ struct TradeVolumeAtPrice
         }
         else
         {
-            total_sell_volume += trade.quantity;
+            total_sell_volume += trade.quantity * -1.0;
         }
         trades.push(trade);
     }
