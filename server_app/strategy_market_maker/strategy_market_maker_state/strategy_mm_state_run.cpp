b9f33bb171f14bd2585cfdc0d260e93140931b0a
huutam1991 <huutam1991@gmail.com>
2025-10-07 23:27:21 +0800

    Reduce default volume in remove_old_trades from 1.0 to 0.1 for improved trade management

--- server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_run.cpp
+++ server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_run.cpp
@@ -182,7 +182,7 @@ Task<void> StrategyMarketMakerStateRun::remove_old_trades()
     {
         // double skew_ratio = ((max_volume - min_volume) / max_volume) * 100.0;
         // m_volume = skew_ratio < 10.0 ? 1.0 : Utils::smooth_curve(max_volume);
-        m_volume = 1.0;
+        m_volume = 0.1;
     }
 
     m_volume *= m_config.volumn;
