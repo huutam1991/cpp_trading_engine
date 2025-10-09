b1fbd37285af98c7ad65a5a63fc06d1f6cceed3e
huutam1991 <huutam1991@gmail.com>
2025-10-09 16:05:50 +0800

    Refactor remove_old_trades to use std::pow for calculating minimum trade volume based on volume ratio

--- server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_run.cpp
+++ server_app/strategy_market_maker/strategy_market_maker_state/strategy_mm_state_run.cpp
@@ -201,7 +201,7 @@ Task<void> StrategyMarketMakerStateRun::remove_old_trades()
     double max_volume = std::max(m_total_buy_volume, m_total_sell_volume);
     double min_volume = std::min(m_total_buy_volume, m_total_sell_volume);
     double volume_ratio = max_volume / min_volume;
-    m_min_trade_volume = (max_volume / 100.0) * (volume_ratio * volume_ratio) * m_config.min_trade_volume_step;
+    m_min_trade_volume = (max_volume / 100.0) * std::pow(volume_ratio, m_config.min_trade_volume_step);
 
     // set [m_price_gap]
     if (max_volume < 5.0)
