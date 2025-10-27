09ccb5cc8ee15247859a859a610f3b58f5cbd633
huutam1991 <huutam1991@gmail.com>
2025-10-27 15:18:50 +0800

    Refactor ClientSocket to update handle_io_data method signature to return int and implement buffer handling logic

--- core/system_io/client_socket.cpp
+++ core/system_io/client_socket.cpp
@@ -7,8 +7,14 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-#include "client_socket.h"
 #include <spdlog/spdlog.h>
+#include <https_server/request/http_request.h>
+#include <https_server/route/route_controller.h>
+#include <coroutine/event_base_manager.h>
+
+#include "client_socket.h"
+
+#define BUFFER_SIZE 2048
 
 void ClientSocket::generate_fd()
 {
@@ -42,7 +48,101 @@ void ClientSocket::generate_fd()
     fcntl(fd, F_SETFL, O_NONBLOCK);
 }
 
-void ClientSocket::handle_io_data()
+int ClientSocket::handle_io_data()
 {
+    char buffer[BUFFER_SIZE];
+    char temp_buffer[BUFFER_SIZE];
+    int read_bytes = 0;
+    int buffer_length = 0;
 
+    if ((read_bytes = read_buffer(temp_buffer)) > 0)
+    {
+        memcpy(buffer + buffer_length, temp_buffer, read_bytes);
+        buffer_length += read_bytes;
+
+        while ((read_bytes = read_buffer(temp_buffer)) > 0)
+        {
+            memcpy(buffer + buffer_length, temp_buffer, read_bytes);
+            buffer_length += read_bytes;
+        }
+
+        if (read_bytes == 0)
+        {
+            spdlog::debug("Connection lost, fd = {}", fd);
+            // Clean save buffer
+            save_buffer = "";
+            return -1;
+        }
+
+        buffer[buffer_length] = '\0';
+    }
+    else
+    {
+        spdlog::debug("Connection lost, fd = {}", fd);
+        // Clean save buffer
+        save_buffer = "";
+        return -1;
+    }
+
+    std::string message = save_buffer + std::string(buffer);
+    HttpRequest* request = HttpRequest::CreateNewHttpRequest(message.c_str(), "web_data"); // Temporarily hard code path: web_data
+
+    // Check if request is nullptr (wrong format), return error 404
+    if (request == nullptr)
+    {
+        // Execute on a single thread
+        auto task = send_404_response(request);
+        task.start_running_on(EventBaseManager::get_event_base_by_id(0)); // Temporarily hard code 0 here, should update later
+
+        return 0;
+    }
+
+    // Post request is invalid format when it's is separated into 2 buffer, that's why we need to save the first buffer
+    if (request->is_valid_format() == false)
+    {
+        save_buffer += std::string(buffer);
+
+        delete request;
+        return 0;
+    }
+
+    // Otherwise, clean save buffer + and execute request
+    save_buffer = "";
+
+    // Execute request on a single thread
+    auto task = execute_request(request);
+    task.start_running_on(EventBaseManager::get_event_base_by_id(0)); // Temporarily hard code 0 here, should update later
+
+    return 0;
+}
+
+int ClientSocket::read_buffer(char* const buffer)
+{
+    return read(fd, buffer, BUFFER_SIZE);
+}
+
+void ClientSocket::write_to_socket_io(const char* buffer, std::uint32_t size)
+{
+    write(fd, buffer, size);
+}
+
+Task<void> ClientSocket::send_404_response(HttpRequest* request)
+{
+    std::string response = request->response_not_found_404().get_response_in_string();
+    write_to_socket_io(response.c_str(), response.size());
+
+    // Clean save buffer
+    save_buffer = "";
+
+    co_return;
+}
+
+Task<void> ClientSocket::execute_request(HttpRequest* request)
+{
+    std::string response = co_await RouteController::instance().handle_request_base_on_route(request);
+    write_to_socket_io(response.c_str(), response.size());
+
+    delete request;
+
+    co_return;
 }
--- core/system_io/client_socket.h
+++ core/system_io/client_socket.h
@@ -1,14 +1,23 @@
 #pragma once
 
+#include <coroutine/task.h>
 #include "system_io_object.h"
 
 struct ClientSocket : public SystemIOObject
 {
     int server_fd;
+    std::string save_buffer;
 
     ClientSocket(int server_fd_value) : server_fd{server_fd_value} {}
 
     // SystemIOObject's methods
     virtual void generate_fd();
-    virtual void handle_io_data();
+    virtual int handle_io_data();
+
+    // Handle data methods
+    virtual int read_buffer(char* const buffer);
+    virtual void write_to_socket_io(const char* buffer, std::uint32_t size);
+
+    Task<void> send_404_response(HttpRequest* request);
+    Task<void> execute_request(HttpRequest* request);
 };
