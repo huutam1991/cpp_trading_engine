b1e5030883852092d3833f010de94a7ecb1e9fe5
huutam1991 <huutam1991@gmail.com>
2025-10-29 20:11:13 +0800

    Enhance IOCPool to pin threads to specific cores for improved performance

--- core/ioc_pool.h
+++ core/ioc_pool.h
@@ -5,9 +5,11 @@
 #include <unordered_map>
 #include <boost/asio.hpp>
 
+#include <utils/thread_pinning.h>
+
 namespace net = boost::asio;
 
-class IOCPool 
+class IOCPool
 {
 public:
     static net::io_context& get_ioc_by_id(size_t id)
@@ -17,20 +19,22 @@ public:
         static std::unordered_map<size_t, std::shared_ptr<net::io_context>> ioc_list;
         static std::unordered_map<size_t, std::shared_ptr<net::executor_work_guard<net::io_context::executor_type>>> guards;
 
-        std::lock_guard<std::mutex> lock(mutex); 
+        std::lock_guard<std::mutex> lock(mutex);
 
         if (ioc_list.find(id) == ioc_list.end())
         {
             // ioc
             auto ioc = std::make_shared<net::io_context>(id);
             ioc_list.insert(std::make_pair(id, ioc));
-            
+
             // guard
             auto guard = std::make_shared<net::executor_work_guard<net::io_context::executor_type>>(net::make_work_guard(*ioc));
             guards[id] = guard;
 
-            threads.emplace_back([ioc]()
+            threads.emplace_back([ioc, id]()
             {
+                // Pin each IOC thread to a specific core
+                ThreadPinning::pin_thread_to_core(static_cast<int>(id));
                 ioc->run();
             });
         }
