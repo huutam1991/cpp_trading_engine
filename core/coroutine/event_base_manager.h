c08cb6512276548a7ae513914ca83284208406ab
huutam1991 <huutam1991@gmail.com>
2025-10-29 20:08:00 +0800

    Add thread pinning to EventBaseManager for core affinity

--- core/coroutine/event_base_manager.h
+++ core/coroutine/event_base_manager.h
@@ -6,6 +6,7 @@
 
 #include <utils/util_macros.h>
 #include <utils/spin_lock.h>
+#include <utils/thread_pinning.h>
 #include <enum_reflect/enum_reflect.h>
 #include "event_base.h"
 #include "epoll_base.h"
@@ -37,6 +38,8 @@ public:
             event_base_list.insert(std::make_pair(id, event_base));
             threads.emplace_back([event_base]()
             {
+                // Pin each event base thread to a specific core
+                ThreadPinning::pin_thread_to_core(static_cast<int>(event_base->m_event_base_id));
                 event_base->loop();
             });
         }
