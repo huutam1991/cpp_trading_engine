a927799a3cc787be4eeb1298a7d0e98adb8c00be
huutam1991 <huutam1991@gmail.com>
2025-11-06 16:03:49 +0800

    Remove DBHelper initialization from main function

--- server_app/main.cpp
+++ server_app/main.cpp
@@ -59,9 +59,6 @@ int main(int argc, char **argv) {
     // Init Timer with ioc TIMER
     Timer::init((EpollBase*)EventBaseManager::get_event_base_by_id(EpollBaseID::SYSTEM_IO_TASK));
 
-    // Init DBHelper with
-    DBHelper::init((EpollBase*)EventBaseManager::get_event_base_by_id(EpollBaseID::SYSTEM_IO_TASK));
-
     GatewayManager::instance().init();
     OrderManager::instance().init();
     SimulatorOrder::init();
