f1c304022940a29de75a1a9494b53821f0ed4b9c
huutam1991 <huutam1991@gmail.com>
2025-11-30 14:47:21 +0800

    Add TLS context classes for secure server and client communication

---
+++ core/network/tls_wrapper/tls_context.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <string>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#include <spdlog/spdlog.h>
+
+class TlsContext
+{
+public:
+    SSL_CTX* ctx = nullptr;
+
+    TlsContext() = default;
+
+    ~TlsContext()
+    {
+        cleanup();
+    }
+
+    // Disallow copying (SSL_CTX cannot be copied)
+    TlsContext(const TlsContext&) = delete;
+    TlsContext& operator=(const TlsContext&) = delete;
+
+    // Allow moving
+    TlsContext(TlsContext&& other) noexcept
+    {
+        ctx = other.ctx;
+        other.ctx = nullptr;
+    }
+
+    TlsContext& operator=(TlsContext&& other) noexcept
+    {
+        if (this != &other)
+        {
+            cleanup();
+            ctx = other.ctx;
+            other.ctx = nullptr;
+        }
+        return *this;
+    }
+protected:
+    static void log_openssl_error(const std::string& msg)
+    {
+        spdlog::error("TLSContext: {}", msg);
+        ERR_print_errors_fp(stderr);
+    }
+
+    // Init methods
+    virtual bool init() = 0;
+
+    void cleanup()
+    {
+        if (ctx)
+        {
+            SSL_CTX_free(ctx);
+            ctx = nullptr;
+        }
+    }
+};
+
+class TlsServerContext : public TlsContext
+{
+    std::string m_cert_file;
+    std::string m_key_file;
+
+public:
+    TlsServerContext(const std::string& cert_file, const std::string& key_file);
+    virtual bool init() override;
+};
+
+class TlsClientContext : public TlsContext
+{
+    bool m_verify_peer;
+    std::string m_ca_file;
+
+public:
+    TlsClientContext(bool verify_peer = false, const std::string& ca_file = "");
+    virtual bool init() override;
+};
