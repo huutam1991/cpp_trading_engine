64f1a6bcfb5714421f7323fd837507ae1dcb5225
huutam1991 <huutam1991@gmail.com>
2025-12-01 14:58:13 +0800

    Enhance logging in HttpClientRequestIO::generate_fd and improve TLS handshake handling

--- core/system_io/https_client_request_io/https_client_request_io.cpp
+++ core/system_io/https_client_request_io/https_client_request_io.cpp
@@ -13,24 +13,30 @@ int HttpClientRequestIO::generate_fd()
     fd = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (fd < 0) return -1;
 
+    spdlog::info("HttpClientRequestIO::generate_fd - 1");
+
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
+    spdlog::info("HttpClientRequestIO::generate_fd - 2");
 
     int res = ::connect(fd, (sockaddr*)&addr, sizeof(addr));
+    spdlog::info("HttpClientRequestIO::generate_fd - 3");
 
     if (res < 0)
     {
         if (errno == EINPROGRESS)
         {
             // Non-blocking connect in progress
+    spdlog::info("HttpClientRequestIO::generate_fd - 4");
             return fd;
         }
 
         close(fd);
         return -1;
     }
+    spdlog::info("HttpClientRequestIO::generate_fd - 5");
 
     return fd;
 }
@@ -63,8 +69,8 @@ int HttpClientRequestIO::handle_io_data()
     // Check to TLS handshake non-blocking
     if (m_tls_wrapper->is_handshake_done() == false)
     {
-        m_tls_wrapper->handshake();
-        return 0;
+        TlsResult result = m_tls_wrapper->handshake();
+        return result != TlsResult::ERROR ? 0 : -1;
     }
     else
     {
