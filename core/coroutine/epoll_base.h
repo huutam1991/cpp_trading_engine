772c5c38ae6eb4abffb64c18ab0a45392c150298
huutam1991 <huutam1991@gmail.com>
2025-12-02 23:03:05 +0800

    Refactor EpollBase::del_fd to improve method declaration consistency

--- core/coroutine/epoll_base.h
+++ core/coroutine/epoll_base.h
@@ -10,11 +10,11 @@ class EpollBase : public EventBase
     int m_epoll_fd;
 
     void add_fd(int fd, SystemIOObject* ptr);
-    void del_fd(SystemIOObject* ptr);
 
 public:
     EpollBase(size_t id);
 
+    void del_fd(SystemIOObject* ptr);
     void start_living_system_io_object(SystemIOObject* object);
     virtual void set_ready_task(void* task_info);
     virtual void loop();
