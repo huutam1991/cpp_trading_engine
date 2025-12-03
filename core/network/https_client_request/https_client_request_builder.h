3e457102383ba4eb29334279c8a55cc85f07dc24
huutam1991 <huutam1991@gmail.com>
2025-12-03 20:21:08 +0800

    Add RequestBuilder struct for efficient HTTP request construction

---
+++ core/network/https_client_request/https_client_request_builder.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+struct RequestBuilder
+{
+    char buf[2048];
+    size_t len = 0;
+
+    template<typename T>
+    inline void append(const T& s)
+    {
+        if constexpr (requires { s.size(); })
+        {
+            size_t n = s.size();
+            memcpy(buf + len, s.data(), n);
+            len += n;
+        }
+        else
+        {
+            constexpr size_t N = sizeof(T);
+            memcpy(buf + len, s, N - 1);
+            len += (N - 1);
+        }
+    }
+
+    inline std::string to_string()
+    {
+        std::string res(buf, len);
+        len = 0;
+        return res;
+    }
+};
