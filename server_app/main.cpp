092a8a73b18f2335028f8e90f07babcd7cffac73
huutam1991 <huutam1991@gmail.com>
2025-12-05 21:49:30 +0800

    Uncomment initialization of GatewayManager, OrderManager, SimulatorOrder, and StrategyManager in main function

--- server_app/main.cpp
+++ server_app/main.cpp
@@ -74,12 +74,12 @@ int main(int argc, char **argv) {
     // Init SpdLog format
     LogInit::init();
 
-    // GatewayManager::instance().init();
-    // OrderManager::instance().init();
-    // SimulatorOrder::init();
+    GatewayManager::instance().init();
+    OrderManager::instance().init();
+    SimulatorOrder::init();
 
-    // // Strategy
-    // StrategyManager::instance().init();
+    // Strategy
+    StrategyManager::instance().init();
 
     // Start HTTPS server - running on EpollBase
     EpollBase* epoll_base = (EpollBase*)EventBaseManager::get_event_base_by_id(EpollBaseID::SYSTEM_IO_TASK);
