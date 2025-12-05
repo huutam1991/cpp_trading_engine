031a3fd72d3b0bb7d8abae56e92dbda54a13f149
huutam1991 <huutam1991@gmail.com>
2025-12-05 21:02:50 +0800

    Implement get_io_events method in HttpServerSocket and HttpsServerSocket for consistent event handling

--- core/system_io/https_server_io/https_server_socket.h
+++ core/system_io/https_server_io/https_server_socket.h
@@ -15,6 +15,7 @@ struct HttpsServerSocket : public HttpServerSocket
 
     // SystemIOObject's methods
     virtual int generate_fd() override;
+    virtual int get_io_events() override { return EPOLLIN; }
     virtual int activate() override;
     virtual int handle_read() override;
     virtual int handle_write() override;
