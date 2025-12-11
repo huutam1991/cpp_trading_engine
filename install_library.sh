60f825514342ed9feafdae5a2a6eac6d7356cce2
huutam1991 <huutam1991@gmail.com>
2025-12-11 16:29:25 +0800

    Fix install_library.sh to remove sudo for npm and correct directory paths for MongoDB drivers

--- install_library.sh
+++ install_library.sh
@@ -49,21 +49,22 @@ install_dependency_packages() {
     apt install -y libspdlog-dev
 
     # Install PM2
-    curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+    curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
     apt install -y nodejs
     node -v
     npm -v
-    sudo npm install -g pm2
+    npm install -g pm2
     pm2 -v
 }
 
 install_dependency_packages
 
+cd "$INSTALL_FOLDER"
 wget https://github.com/mongodb/mongo-c-driver/releases/download/1.23.0/mongo-c-driver-1.23.0.tar.gz
 tar xzf mongo-c-driver-1.23.0.tar.gz
-cd /mongo-c-driver-1.23.0
+cd "$INSTALL_FOLDER"/mongo-c-driver-1.23.0
 mkdir cmake-build
-cd /mongo-c-driver-1.23.0/cmake-build
+cd "$INSTALL_FOLDER"/mongo-c-driver-1.23.0/cmake-build
 pwd
 cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
 cmake -DMONGOC_TEST_USE_CRYPT_SHARED=OFF ..
@@ -71,32 +72,34 @@ cmake --build . -j 4
 cmake --build . --target install
 
 # Install the mongocxx driver
+cd "$INSTALL_FOLDER"
 curl -OL https://github.com/mongodb/mongo-cxx-driver/releases/download/r3.6.7/mongo-cxx-driver-r3.6.7.tar.gz
 tar -xzf mongo-cxx-driver-r3.6.7.tar.gz
 # Patch all .hpp files that use std::uintXX_t or std::intXX_t but are missing <cstdint> (not nice, but works)
-find /mongo-cxx-driver-r3.6.7/src -name "*.hpp" | while read file; do
+find "$INSTALL_FOLDER"/mongo-cxx-driver-r3.6.7/src -name "*.hpp" | while read file; do
   if grep -qE 'std::(u?int(8|16|32|64)_t)' "$file" && ! grep -q '<cstdint>' "$file"; then
     echo "Patching $file"
     sed -i '/#pragma once/a #include <cstdint>' "$file"
   fi
 done
-cd /mongo-cxx-driver-r3.6.7/build
+cd "$INSTALL_FOLDER"/mongo-cxx-driver-r3.6.7/build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build . -j 4
 cmake --build . --target install
 
 # Install google test
+cd "$INSTALL_FOLDER"
 git clone https://github.com/google/googletest.git
-cd googletest
+cd "$INSTALL_FOLDER"/googletest
 cmake -B build -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build build
 cmake --install build
 
 # # Install GLog
-# cd /
+# cd "$INSTALL_FOLDER"
 # git clone https://github.com/google/glog.git
-# cd glog
-# mkdir build && cd build
+# cd "$INSTALL_FOLDER"/glog
+# mkdir build && cd "$INSTALL_FOLDER"/build
 # cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..
 # make -j$(nproc) && make install
 
