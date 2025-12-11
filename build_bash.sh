570669d6ff0b891e821b040724b8add60ca3a58c
huutam1991 <huutam1991@gmail.com>
2025-12-11 18:43:05 +0800

    Fix CMake flags in build_bash.sh to specify correct optimization level

--- build_bash.sh
+++ build_bash.sh
@@ -6,8 +6,8 @@ cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CXX_COMPILER=g++ \
   -DCMAKE_C_COMPILER=gcc \
-  -DCMAKE_CXX_FLAGS="-O3 -march=native -mtune=native -flto -ffast-math -fno-plt" \
-  -DCMAKE_C_FLAGS="-O3 -march=native -mtune=native -flto -ffast-math -fno-plt" \
+  -DCMAKE_CXX_FLAGS="-O3 -march=native -mtune=native -flto=8 -ffast-math -fno-plt" \
+  -DCMAKE_C_FLAGS="-O3 -march=native -mtune=native -flto=8 -ffast-math -fno-plt" \
   -DENABLE_UNITY_BUILD=OFF \
   -DBUILD_ONLY="core;sts;identitystore;s3;ec2"
 cmake --build . -j 6
