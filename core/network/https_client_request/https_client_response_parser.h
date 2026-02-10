4522945c170cec748a0d96d70a386f7ae715152e
huutam1991 <huutam1991@gmail.com>
2026-02-10 16:32:31 +0800

    Move HttpsClientResponse struct definition into HttpsClientResponseParser header

--- core/network/https_client_request/https_client_response_parser.h
+++ core/network/https_client_request/https_client_response_parser.h
@@ -1,6 +1,19 @@
 #pragma once
 
-#include "https_client_response.h"
+#include <string>
+#include <sstream>
+#include <strings.h>
+#include <unordered_map>
+
+struct HttpsClientResponse
+{
+    int status_code = 0;
+    std::string status_message;
+    std::unordered_map<std::string, std::string> headers;
+    std::string body;
+
+    bool is_complete = false;
+};
 
 class HttpsClientResponseParser
 {
