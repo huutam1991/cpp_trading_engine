76be5ed5f2f4ef5ce67baa8f07054352b5d9aab3
huutam1991 <huutam1991@gmail.com>
2026-02-11 16:07:07 +0800

    Refactor keep_listen_key to use HttpsClientRequest and improve response handling

--- server_app/gateways/binance/binance_quoter/binance_quoter_spot.cpp
+++ server_app/gateways/binance/binance_quoter/binance_quoter_spot.cpp
@@ -196,11 +196,11 @@ Task<std::string> BinanceQuoterSpot::get_listen_key()
 
 Task<void> BinanceQuoterSpot::keep_listen_key()
 {
-    auto client = std::make_shared<HttpsClientAsync>(IOCPool::get_ioc_by_id(IOCId::ORDER_ENTRY), m_url, m_port);
-    client->add_header("X-MBX-APIKEY", m_api_key);
+    HttpsClientRequest client(m_epoll_base, m_url, std::stoi(m_port));
+    client.add_header("X-MBX-APIKEY", m_api_key);
 
-    std::string str = co_await client->put("/api/v3/userDataStream?listenKey=" + m_listen_key, "");
-    Json data = Json::parse(str);
+    HttpsClientResponse response = co_await client.put("/api/v3/userDataStream?listenKey=" + m_listen_key, "");
+    Json data = Json::parse(response.body);
 
     spdlog::debug("BinanceQuoterSpot, re-active m_listen_key = {}", m_listen_key);
 
