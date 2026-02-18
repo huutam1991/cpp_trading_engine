7c6021fc2097c63047caf2a30d28478e45d4ac75
huutam1991 <huutam1991@gmail.com>
2026-02-18 15:25:28 +0700

    Simplify pointer checks in JsonTypeBase methods for improved readability

--- core/json/json_type_base.h
+++ core/json/json_type_base.h
@@ -31,44 +31,26 @@ protected:
 public:
     inline bool is_json_value()
     {
-        if (is_json_value_ptr != nullptr)
-        {
-            return (this->*is_json_value_ptr)();
-        }
-        return false;
+        return (this->*is_json_value_ptr)();
     }
 
     inline JsonTypeBase* get_copy()
     {
-        if (get_copy_ptr != nullptr)
-        {
-            return (this->*get_copy_ptr)();
-        }
-        return nullptr;
+        return (this->*get_copy_ptr)();
     }
 
     inline JsonTypeBase* get_deep_clone()
     {
-        if (get_deep_clone_ptr != nullptr)
-        {
-            return (this->*get_deep_clone_ptr)();
-        }
-        return nullptr;
+        return (this->*get_deep_clone_ptr)();
     }
 
     inline void write_string_value(JsonStringBuilder& builder)
     {
-        if (write_string_value_ptr != nullptr)
-        {
-            (this->*write_string_value_ptr)(builder);
-        }
+        (this->*write_string_value_ptr)(builder);
     }
 
     inline void release()
     {
-        if (release_ptr != nullptr)
-        {
-            (this->*release_ptr)();
-        }
+        (this->*release_ptr)();
     }
 };
