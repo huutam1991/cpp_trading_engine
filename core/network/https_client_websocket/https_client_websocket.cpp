bcbe2c039087bb0f98100140b4c455eae0660c08
huutam1991 <huutam1991@gmail.com>
2026-03-12 17:47:48 +0800

    Implement websocket connection handling in HttpsClientWebsocket class

--- core/network/https_client_websocket/https_client_websocket.cpp
+++ core/network/https_client_websocket/https_client_websocket.cpp
@@ -1,6 +1,40 @@
 #include "https_client_websocket.h"
 
 HttpsClientWebsocket::HttpsClientWebsocket(EpollBase* epoll_base, const std::string& hostname, int port)
-    : m_connection{epoll_base, hostname, port}
+    : m_tcp_connection(std::make_unique<TCPConnection>(epoll_base, hostname, port)),
+      m_rest_request(epoll_base, hostname, port, std::move(m_tcp_connection)),
+      m_connection{epoll_base, hostname, port}
 {
+    connect().start_running_on(epoll_base);
+}
+
+Task<void> HttpsClientWebsocket::connect()
+{
+    // wss://echo.websocket.events
+
+    // GET / HTTP/1.1
+    // Host: echo.websocket.events
+    // Upgrade: websocket
+    // Connection: Upgrade
+    // Sec-WebSocket-Key: <random_base64>
+    // Sec-WebSocket-Version: 13
+
+    m_rest_request.add_header("Upgrade", "websocket");
+    m_rest_request.add_header("Connection", "Upgrade");
+    m_rest_request.add_header("Sec-WebSocket-Key", "dGhlIHNhbXBsZSBub25jZQ==");
+    m_rest_request.add_header("Sec-WebSocket-Version", "13");
+
+    HttpsClientResponse response = co_await m_rest_request.get("/");
+
+    if (response.status_code == 101)
+    {
+        spdlog::info("Websocket connection established");
+    }
+    else
+    {
+        spdlog::error("Failed to establish websocket connection, status code: {}", response.status_code);
+        co_return;
+    }
+
+    co_return;
 }
--- core/network/https_client_websocket/https_client_websocket.h
+++ core/network/https_client_websocket/https_client_websocket.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <network/tcp_connection/tcp_connection.h>
+#include <network/https_client_request/https_client_request.h>
+
 #include "https_client_websocket_connection.h"
 
 class HttpsClientWebsocket
 {
+    std::unique_ptr<TCPConnection> m_tcp_connection = nullptr;
+    HttpsClientRequest m_rest_request;
     HttpsClientWebsocketConnection m_connection;
 
+public:
     HttpsClientWebsocket(EpollBase* epoll_base, const std::string& hostname, int port);
+
+private:
+    Task<void> connect();
 };
