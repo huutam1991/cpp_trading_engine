050fdbd9a5476119efb4a253fe7d08895c5c0c63
huutam1991 <huutam1991@gmail.com>
2026-03-15 21:16:59 +0800

    Enhance update method in StrategyMeanReversionStateStop to improve logging for price updates and order book snapshots

--- server_app/strategy_mean_reversion/strategy_mean_reversion_state/strategy_mr_state_stop.cpp
+++ server_app/strategy_mean_reversion/strategy_mean_reversion_state/strategy_mr_state_stop.cpp
@@ -28,9 +28,22 @@ Task<void> StrategyMeanReversionStateStop::update(StrategyUpdateData data)
     if (std::holds_alternative<PriceUpdate>(data))
     {
         price_update = std::get<PriceUpdate>(data);
+        spdlog::debug("StrategyMeanReversionStateStop: do nothing, symbol: {}, price: {}", price_update.instrument->symbol, price_update.price);
     }
+    else if (std::holds_alternative<OrderBookSnapShot*>(data))
+    {
+        OrderBookSnapShot* snapshot = std::get<OrderBookSnapShot*>(data);
+        double bid_price = snapshot->get_best_bid();
+        double ask_price = snapshot->get_best_ask();
+        double ask_quantity = snapshot->get_best_ask_quantity();
+        double bid_quantity = snapshot->get_best_bid_quantity();
+        // m_pnl.update_current_price((bid_price + ask_price) / 2.0);
 
-    spdlog::debug("StrategyMeanReversionStateStop: do nothing, symbol: {}, price: {}", price_update.instrument->symbol, price_update.price);
+        spdlog::info("StrategyMarketMakerStateStop: do nothing, symbol: {}, bid_price: {}, ask_price: {}", snapshot->instrument->symbol, bid_price, ask_price);
+        spdlog::info("StrategyMarketMakerStateStop: do nothing, symbol: {}, bid_quantity: {}, ask_quantity: {}", snapshot->instrument->symbol, bid_quantity, ask_quantity);
+
+        OrderBookSnapShotPool::release(snapshot);
+    }
 
     co_return;
 }
