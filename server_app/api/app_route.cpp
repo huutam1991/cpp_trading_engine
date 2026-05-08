96cf347d3c3aadd6989690ea8178b2cb14cf7e8f
huutam1991 <huutam1991@gmail.com>
2026-05-08 20:10:58 +0800

    fix: update CLIENT_DEPLOY_FOLDER path to reflect Vue.js UI directory

--- server_app/api/app_route.cpp
+++ server_app/api/app_route.cpp
@@ -26,7 +26,7 @@
 
 #include <gateways/gateway_manager.h>
 
-std::string CLIENT_DEPLOY_FOLDER = "angular_src/dist/vue_ui";
+std::string CLIENT_DEPLOY_FOLDER = "vuejs_ui/dist";
 
 using namespace std;
 
