60b5444b4c3e925c9a42ada462ec35be11ba461c
huutam1991 <huutam1991@gmail.com>
2026-05-25 17:14:28 +0800

    fix: comment out assignment of m_package_last_update_id in process_buffered_updates_after_snapshot method

--- server_app/gateways/binance/binance_market_data/order_book/binance_order_book.cpp
+++ server_app/gateways/binance/binance_market_data/order_book/binance_order_book.cpp
@@ -228,7 +228,7 @@ bool BinanceOrderBook::process_buffered_updates_after_snapshot()
         // no buffered updates newer than snapshot
         // snapshot itself is already latest
 
-        m_package_last_update_id = m_snapshot_last_update_id;
+        // m_package_last_update_id = m_snapshot_last_update_id;
 
         spdlog::info(
             "Snapshot already up to date for symbol [{}], snapshot_last_update_id={}",
