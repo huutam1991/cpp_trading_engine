9978b96a9d5c0ee99e47f9197f8012f6eb2a5115
huutam1991 <huutam1991@gmail.com>
2026-06-11 19:46:52 +0800

    feat: remove debug logging from HttpRequest constructor

--- core/network/https_server/request/http_request.cpp
+++ core/network/https_server/request/http_request.cpp
@@ -32,7 +32,6 @@ std::function<HttpResponse(HttpRequest*)> HttpRequest::s_bad_request_getter = ba
 
 HttpRequest::HttpRequest(const std::string& content, const std::string& dir_path) : m_dir_path(dir_path)
 {
-    spdlog::debug("HttpRequest content = \n{}", content);
     deserialize(content);
 }
 
