eefe7b57e98692bb69272fca74ed4985889e0efa
huutam1991 <huutam1991@gmail.com>
2026-06-18 16:33:07 +0800

    refactor: remove global_metric.h and integrate flow_tracing.h for metric management in TraceTransfer

--- core/metric/trace_transfer.h
+++ core/metric/trace_transfer.h
@@ -3,13 +3,15 @@
 #include <utils/utils.h>
 #include <coroutine/event_base_manager.h>
 
+#include "flow_tracing.h"
+
 struct TraceTransfer
 {
     EventBaseID from;
     EventBaseID to;
 
     uint64_t enqueue_ns;
-    uint64_t execute_ns;
+    uint64_t delay_ns;
 
     void record_enqueue(EventBaseID to_id)
     {
@@ -21,6 +23,14 @@ struct TraceTransfer
 
     void record_execute()
     {
-        execute_ns = Utils::get_time_now_in_utc_nanoseconds() - enqueue_ns;
+        delay_ns = Utils::get_time_now_in_utc_nanoseconds() - enqueue_ns;
+        update_metric();
+    }
+
+    void update_metric()
+    {
+        auto& metric = FlowTracing::metric(from, to);
+        metric.count.fetch_add(1, std::memory_order_relaxed);
+        metric.total_delay_ns.fetch_add(delay_ns, std::memory_order_relaxed);
     }
 };
