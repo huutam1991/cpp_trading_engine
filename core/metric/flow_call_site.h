a18b9f023acaec3f39de70c1c6086b8115bb3e9f
huutam1991 <huutam1991@gmail.com>
2026-06-19 23:42:46 +0800

    feat: implement get_json_data method in FlowTracing to aggregate JSON data from all call sites

--- core/metric/flow_call_site.h
+++ core/metric/flow_call_site.h
@@ -61,3 +61,27 @@ struct FlowCallSiteKey
 {
     static inline FlowCallSiteRegister<File, Function, Line> register_instance;
 };
+
+Json FlowTracing::get_json_data()
+{
+    Json data;
+
+    for (const FlowCallSite& callsite : FlowCallSiteManager::all_call_sites())
+    {
+        Json callsite_data = callsite.get_json_data();
+        if (callsite_data == nullptr)
+        {
+            continue; // Skip empty data
+        }
+
+        callsite_data.for_each_with_key([&](const std::string& from, Json& from_data)
+        {
+            from_data.for_each_with_key([&](const std::string& to, Json& metric_data)
+            {
+                data[from][to].push_back(metric_data);
+            });
+        });
+    }
+
+    return data;
+}
