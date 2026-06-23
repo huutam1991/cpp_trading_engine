b55ef0ace071c9ffff39f6341d2396b19ee173db
huutam1991 <huutam1991@gmail.com>
2026-06-23 19:37:48 +0800

    feat: implement API handler for instrument list retrieval

---
+++ server_app/api_handler/api_handler_instrument/api_handler_instrument_list.cpp
@@ -0,0 +1,39 @@
+#include <api_handler/api_handler_instrument/api_handler_instrument_list.h>
+#include <spdlog/spdlog.h>
+#include <mongo_db/mongo_db.h>
+#include <instrument/instrument.h>
+
+APIHandlerInstrumentList::APIHandlerInstrumentList(HttpRequest* request) : APIHandler(request)
+{
+    m_need_check_authentication = true;
+}
+
+Task<HttpResponse> APIHandlerInstrumentList::child_handle()
+{
+    Json data;
+
+    std::vector<std::string> collections = MongoDB::instance().get_collection_name_list("instrument");
+    for (const std::string& collection_name : collections)
+    {
+        if (collection_name != "subscribed_instruments")
+        {
+            Json instruments= MongoDB::instance()
+                .set_db_and_collection("instrument", collection_name)
+                .find_many();
+
+            instruments.for_each([&data](Json& instrument)
+            {
+                data.push_back(instrument);
+            });
+        }
+    }
+
+    // Response
+    Json response;
+    response["orders"] = data;
+    response["msg"] = "Get instrument list successfully";
+    response["status_code"] = OK_200;
+    response["error"] = false;
+
+    co_return HttpResponse(OK_200, response);
+}
