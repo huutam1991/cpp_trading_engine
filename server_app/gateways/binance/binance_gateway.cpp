5dd35b7b8ae5f774c0dcbb7521071385622cd27a
huutam1991 <huutam1991@gmail.com>
2026-06-25 03:05:43 +0800

    refactor: validate positions response in get_positions method

--- server_app/gateways/binance/binance_gateway.cpp
+++ server_app/gateways/binance/binance_gateway.cpp
@@ -280,16 +280,16 @@ Task<Json> BinanceGateway::get_balances()
 
 Task<Json> BinanceGateway::get_positions()
 {
-    Json data;
-
     Json positions = co_await m_quoter_perpetual.get_positions();
-    spdlog::warn("positions: {}", positions);
+    if (positions.is_array() == false)
+    {
+        co_return {};
+    }
+
+    Json data;
 
     positions.for_each([&data](Json& position)
     {
-        spdlog::warn("symbol of position: {}", (std::string)position["symbol"]);
-        spdlog::warn("position: {}", position);
-
         const Instrument* instrument = Instrument::get_instrument_by_exchange_symbol(
             ExchangeId::BINANCE,
             InstrumentType::PERPETUAL,
