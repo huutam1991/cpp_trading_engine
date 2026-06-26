b7e81317a3e1578e236c3b66acf0073fb7a6ae78
huutam1991 <huutam1991@gmail.com>
2026-06-27 03:05:46 +0800

    feat: implement APIHandlerAccountList for retrieving account list

---
+++ server_app/api_handler/api_handler_account/api_handler_account_list.cpp
@@ -0,0 +1,19 @@
+#include <api_handler/api_handler_account/api_handler_account_list.h>
+#include <account/account.h>
+
+APIHandlerAccountList::APIHandlerAccountList(HttpRequest* request) : APIHandler(request)
+{
+    m_need_check_authentication = true;
+}
+
+Task<HttpResponse> APIHandlerAccountList::child_handle()
+{
+    Json response;
+
+    response["data"] = Account::load_all_accounts();
+    response["msg"] = "Get account list successfully";
+    response["status_code"] = OK_200;
+    response["error"] = false;
+
+    co_return HttpResponse(OK_200, response);
+}
