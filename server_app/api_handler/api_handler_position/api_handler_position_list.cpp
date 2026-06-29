b36683e543369ecbda7e278974b153b346dbb65c
huutam1991 <huutam1991@gmail.com>
2026-06-29 21:43:30 +0800

    feat: update APIHandlerPositionList to retrieve positions from active accounts

--- server_app/api_handler/api_handler_position/api_handler_position_list.cpp
+++ server_app/api_handler/api_handler_position/api_handler_position_list.cpp
@@ -1,5 +1,6 @@
 #include <api_handler/api_handler_position/api_handler_position_list.h>
-#include <gateways/gateway_manager.h>
+#include <account/account_manager.h>
+#include <account/account.h>
 
 APIHandlerPositionList::APIHandlerPositionList(HttpRequest* request) : APIHandler(request)
 {
@@ -8,17 +9,28 @@ APIHandlerPositionList::APIHandlerPositionList(HttpRequest* request) : APIHandle
 
 Task<HttpResponse> APIHandlerPositionList::child_handle()
 {
-    // Only support BINANCE for now
-    auto gateway_binance = GatewayManager::instance().get_gateway(ExchangeId::BINANCE);
+    Json positions;
+
+    auto active_accounts = AccountManager::get_active_accounts();
+    for (std::shared_ptr<AccountBase> account : active_accounts)
+    {
+        Json account_positions = co_await account->m_order_entry->get_positions();
+
+        account_positions.for_each([&](Json& position)
+        {
+            positions.push_back(position);
+        });
+    }
 
     // Response
     Json response;
     // [Tam temporarily comment out - OrderEntry refactor]
     // response["positions"] = co_await gateway_binance->get_positions();
-    response["positions"] = {};
+    response["positions"] = positions;
     response["msg"] = "Get position list successfully";
     response["status_code"] = OK_200;
     response["error"] = false;
+
 
     co_return HttpResponse(OK_200, response);
 }
