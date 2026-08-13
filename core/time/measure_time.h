6ba29e9ab3f32db5ff05c3b04722cdd884f9324d
huutam1991 <huutam1991@gmail.com>
2026-08-13 11:47:11 +0700

    refactor: consolidate MeasureTime class and remove unused MeasureUnit enum

--- core/time/measure_time.h
+++ core/time/measure_time.h
@@ -8,46 +8,6 @@
 #include <enum_reflect/enum_reflect.h>
 #include <utils/util_macros.h>
 
-enum MeasureUnit
-{
-    NANOSECOND = 0,
-    MICROSECOND,
-    MILLISECOND,
-    SECOND
-};
-
-class MeasureTime
-{
-private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start;
-    std::chrono::time_point<std::chrono::high_resolution_clock> end;
-    std::string m_logs;
-    MeasureUnit m_measure_unit;
-    bool m_is_stop = false;
-
-public:
-    MeasureTime(std::string logs, MeasureUnit measure_unit = MeasureUnit::NANOSECOND) : m_logs{logs}, m_measure_unit{measure_unit}
-    {
-        start = std::chrono::high_resolution_clock::now();
-    }
-
-    ~MeasureTime()
-    {
-        end = m_is_stop == false ? std::chrono::high_resolution_clock::now() : end;
-        auto duration_count = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-        double execute_time = (double)duration_count / std::pow(1000.0, m_measure_unit);
-        std::string_view unit = enum_reflect::enum_name(m_measure_unit);
-
-        spdlog::debug("Execute time - {}: {} {}", m_logs, execute_time, unit);
-    }
-
-    void stop_counting()
-    {
-        end = std::chrono::high_resolution_clock::now();
-        m_is_stop = true;
-    }
-};
-
 struct PipelineTiming
 {
     uint64_t ticks{};
@@ -55,7 +15,7 @@ struct PipelineTiming
     double us{};
 };
 
-class MeasurePipelineTime
+class MeasureTime
 {
 public:
     static void init()
@@ -69,13 +29,13 @@ public:
         return tsc_ghz;
     }
 
-    explicit MeasurePipelineTime(const std::string& logs) : m_logs(logs)
+    explicit MeasureTime(const std::string& logs) : m_logs(logs)
     {
         _mm_lfence();
         m_start = __rdtsc();
     }
 
-    ~MeasurePipelineTime()
+    ~MeasureTime()
     {
         unsigned aux;
 
@@ -89,8 +49,8 @@ public:
         spdlog::debug("Execute time - {}: {} ticks, {} ns, {} us", m_logs, m_result.ticks, m_result.ns, m_result.us);
     }
 
-    MeasurePipelineTime(const MeasurePipelineTime&) = delete;
-    MeasurePipelineTime& operator=(const MeasurePipelineTime&) = delete;
+    MeasureTime(const MeasureTime&) = delete;
+    MeasureTime& operator=(const MeasureTime&) = delete;
 
 private:
     std::string m_logs;
