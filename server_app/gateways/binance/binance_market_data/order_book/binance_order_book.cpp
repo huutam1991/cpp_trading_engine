42ed07105d80419b78e6b9c2f808df406cc52e87
huutam1991 <huutam1991@gmail.com>
2026-08-19 16:54:42 +0700

    fix: update market_data_received timing to use record_start_time and record_end_time methods

--- server_app/gateways/binance/binance_market_data/order_book/binance_order_book.cpp
+++ server_app/gateways/binance/binance_market_data/order_book/binance_order_book.cpp
@@ -36,7 +36,7 @@ Task<void> BinanceOrderBook::start_fetching_order_book()
         [this](std::string buffer) -> Task<void>
         {
             m_trace_id = g_pipeline_trace_buffer.allocate();
-            g_pipeline_trace_buffer.get<"market_data_received">(m_trace_id).ticks = MeasureTime::read_tsc();
+            g_pipeline_trace_buffer.get<"market_data_received">(m_trace_id).record_start_time();
 
             if (m_has_received_first_update == false)
             {
@@ -52,6 +52,8 @@ Task<void> BinanceOrderBook::start_fetching_order_book()
 
             Json data = Json::parse(std::move(buffer));
             handle_order_book_update(std::move(data["data"]));
+
+            g_pipeline_trace_buffer.get<"market_data_received">(m_trace_id).record_end_time();
 
             co_return;
         },
