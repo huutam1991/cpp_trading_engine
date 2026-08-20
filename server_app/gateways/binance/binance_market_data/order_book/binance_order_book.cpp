ce8ff224fd4933ecb7323eb2e7dd1b52686eba91
huutam1991 <huutam1991@gmail.com>
2026-08-20 17:41:54 +0700

    fix: update PipelineTraceBuffer usage in order book and strategy state stop

--- server_app/gateways/binance/binance_market_data/order_book/binance_order_book.cpp
+++ server_app/gateways/binance/binance_market_data/order_book/binance_order_book.cpp
@@ -35,7 +35,7 @@ Task<void> BinanceOrderBook::start_fetching_order_book()
         // on_message
         [this](std::string buffer) -> Task<void>
         {
-            m_trace_id = g_pipeline_trace_buffer.allocate();
+            m_trace_id = PipelineTraceBuffer::allocate();
             PipelineTraceBuffer::RecordStageTiming<"market_data_received"> record_stage(m_trace_id);
 
             if (m_has_received_first_update == false)
