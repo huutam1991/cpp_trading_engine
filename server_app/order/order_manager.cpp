7b9e1c2b777cdf258ea57081c31a8c5a8686228b
huutam1991 <huutam1991@gmail.com>
2026-08-25 11:26:23 +0700

    fix: ensure order_db is removed after erasing from caches in update_order_in_db

--- server_app/order/order_manager.cpp
+++ server_app/order/order_manager.cpp
@@ -105,9 +105,10 @@ Task<void> OrderManager::update_order_in_db(Order order)
     // If order is canceled or rejected, remove it from [m_order_db_cache]
     if (order_db->status == Order::Status::CANCELED)
     {
-        order_db.remove();
         m_order_db_cache.erase(order_db->order_id);
         m_order_list.erase(order_db->order_id);
+
+        order_db.remove();
     }
     // For FILLED order, we can also remove it from [m_order_db_cache] to save space, but dont remove from DB
     else if (order_db->status == Order::Status::FILLED)
