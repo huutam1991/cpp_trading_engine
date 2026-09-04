827d6498fa91e07dd0f80f8c048c0beabf41f1b8
huutam1991 <huutam1991@gmail.com>
2026-09-04 14:15:43 +0800

    feat: add route for strategy view in router

--- web_data/vuejs_ui/src/router/index.ts
+++ web_data/vuejs_ui/src/router/index.ts
@@ -43,6 +43,11 @@ const router = createRouter({
           component: () => import('@/views/OrderBookView.vue'),
         },
         {
+          path: 'strategy',
+          name: 'strategy',
+          component: () => import('@/views/StrategyView.vue'),
+        },
+        {
           path: 'order',
           name: 'order',
           component: () => import('@/views/OrderView.vue'),
